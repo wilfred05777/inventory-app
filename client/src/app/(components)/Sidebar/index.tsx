c9a68d943dce6fa8065da3b69dd59139137ccfe7
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-02-01 06:02:44 +0800

    1:16:13 - toggle action in navbar and sidebar - with react-redux paradigm - end

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -1,19 +1,45 @@
 "use client";
 
+import { useAppDispatch, useAppSelector } from "@/app/redux";
+import { setIsSidebarCollapsed } from "@/state";
 import { Menu } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
+   const dispatch = useAppDispatch();
+   const isSidebarCollapsed = useAppSelector(
+      (state) => state.global.isSidebarCollapsed
+   );
+
+   const toggleSidebar = () => {
+      dispatch(setIsSidebarCollapsed(!isSidebarCollapsed));
+   };
+
+   const sidebarClassNames = `fixed flex flex-col ${
+      isSidebarCollapsed ? "w-0 md:w-16" : "w-72 md:w-64"
+   } bg-white transition-all duration-300 overflow-hidden h-full shadow-md z-40`;
+
    return (
-      <div>
+      <div className={sidebarClassNames}>
          {/* TOP Logo */}
-         <div className='flex gap-3 justify-between md:justify-normal items-center pt-8'>
+         <div
+            className={`flex gap-3 justify-between md:justify-normal items-center pt-8 ${
+               isSidebarCollapsed ? "px-5" : "px-8"
+            }`}
+         >
             <div className='logo'>Logo</div>
-            <h1 className='font-extrabold text-2xl'>Invetory Logo</h1>
+            <h1
+               className={`${
+                  isSidebarCollapsed ? "hidden" : "block"
+               }font-extrabold text-2xl`}
+            >
+               RBJMAZ
+            </h1>
 
             <button
                className='md:hidden px-3 py-3 border-gray-100 rounded-full hover:bg-blue-100'
-               onClick={() => {}}
+               // onClick={() => {}}
+               onClick={toggleSidebar}
             >
                <Menu className='w-4 h-4' />
             </button>
