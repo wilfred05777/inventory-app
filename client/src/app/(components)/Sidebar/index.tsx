8e88c06c5a8dd6364be8237f6c4cff546284b9bd
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-02-03 04:51:34 +0800

    SideBarLink - 1:26:19 - End

--- client/src/app/(components)/Sidebar/index.tsx
+++ client/src/app/(components)/Sidebar/index.tsx
@@ -2,8 +2,58 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
-import { Menu } from "lucide-react";
+import {
+   Archive,
+   CircleDollarSign,
+   Clipboard,
+   Layout,
+   LucideIcon,
+   Menu,
+   SlidersHorizontal,
+   User,
+} from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
+
+interface SidebarLinkProps {
+   href: string;
+   icon: LucideIcon;
+   label: string;
+   isCollapsed: boolean;
+}
+
+const SidebarLink = ({
+   href,
+   icon: Icon,
+   label,
+   isCollapsed,
+}: SidebarLinkProps) => {
+   const pathname = usePathname();
+   const isActive =
+      pathname === href || (pathname === "/" && href === "/dashboard");
+
+   return (
+      <Link href={href}>
+         <div
+            className={`cursor-pointer flex items-center ${
+               isCollapsed ? "justify-center py-4" : "justify-start px-8 py-4"
+            } hover:text-blue-500 hover:bg-blue-100 gap-3 transition-colors ${
+               isActive ? "bg-blue-200" : ""
+            }`}
+         >
+            <Icon className='w-6 h-6 !text-gray-700' />
+            <span
+               className={`${
+                  isCollapsed ? "hidden" : "block"
+               } font-medium text-gray-700`}
+            >
+               {label}
+            </span>
+         </div>
+      </Link>
+   );
+};
 
 const Sidebar = () => {
    const dispatch = useAppDispatch();
@@ -45,9 +95,49 @@ const Sidebar = () => {
             </button>
          </div>
          {/* LINKS */}
-         <div className='flex-grow mt-8'>{/* links here */}</div>
-         <div className='footer'>
-            <p>&copy; 2024 Inventory</p>
+         <div className='flex-grow mt-8'>
+            {/* links here */}
+            <SidebarLink
+               href='/dashboard'
+               icon={Layout}
+               label='Dashboard'
+               isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+               href='/invetory'
+               icon={Archive}
+               label='Invetory'
+               isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+               href='/products'
+               icon={Clipboard}
+               label='Products'
+               isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+               href='/users'
+               icon={User}
+               label='Users'
+               isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+               href='/settings'
+               icon={SlidersHorizontal}
+               label='Settings'
+               isCollapsed={isSidebarCollapsed}
+            />
+            <SidebarLink
+               href='/expenses'
+               icon={CircleDollarSign}
+               label='Expenses'
+               isCollapsed={isSidebarCollapsed}
+            />
+         </div>
+         <div className={`${isSidebarCollapsed ? "hidden" : "block"} mb-10`}>
+            <p className='text-center text-xs text-gray-500'>
+               &copy; 2024 Inventory
+            </p>
          </div>
       </div>
    );
