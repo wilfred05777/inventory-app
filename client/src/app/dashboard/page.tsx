1dc88e7693fee0a925548aa9ddb9e799500b5e0b
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-02-06 04:38:54 +0800

    2:26:48-2:35:00 - grid system initial implementation for UI/UX

--- client/src/app/dashboard/page.tsx
+++ client/src/app/dashboard/page.tsx
@@ -1,8 +1,15 @@
-
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+   return (
+      <div className='grid gird-cols-1 md:grid-cols-2 xl:grid-cols-3 xl:overflow-auto gap-10 pb-4 custom-grid-rows'>
+         <div className='row-span-3 xl-row-span-6 bg-gray-500'></div>
+         <div className='row-span-3 xl:row-span-6 bg-gray-500'></div>
+         <div className='row-span-3 xl:row-span-3 col-span-1 md:col-span-2 xl:col-span-1 bg-gray-500'></div>
+         <div className='row-span-3 bg-gray-500' />
+         <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
+         <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
+         <div className='md:row-span-1 xl:row-span-2 bg-gray-500' />
+      </div>
+   );
+};
 
-export default Dashboard
+export default Dashboard;
