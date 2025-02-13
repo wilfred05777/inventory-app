cf738c64b8a33d6ebd266173420e991206e03f6f
Wilfred Erdo Bancairen <wilfred05777@gmail.com>
2025-02-13 19:12:11 +0800

    4:21:31- bug invetory / products not showing or fetching from the database.

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -50,8 +50,8 @@ export interface DashboardMetrics {
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
-  // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000" }),
+  baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_BASE_URL }),
+  // baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8000/" }),
   tagTypes: ["DashboardMetrics", "Products"],
   endpoints: (build) => ({
     getDashboardMetrics: build.query<DashboardMetrics, void>({
